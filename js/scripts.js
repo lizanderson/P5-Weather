b7b234481c3fd5e7f19b8b8ffbf1759c41c62fe6
lizanderson <eanderson4@it-rpub-mac15.eastern.ewu.edu>
2015-03-10 13:44:36 -0700

    Version 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,3 @@
-// REF: http://foundation.zurb.com/docs/
-// REF: http://simpleweatherjs.com/
-
 var cheney = '99004';
 var spokane = '99220';
 
@@ -14,9 +11,90 @@ var spokane = '99220';
     success: function(weather) {
 
       //Get and output weather HERE!!!!
+      $.simpleWeather({
+    
+    location: '99004', // change zip
+    unit: 'f',
+    
+    // Get _weather_ object
+    success: function(weather) {
+      
+      // Get & store temperature
+      var temp = weather.temp;
+      // Get & store city
+      var city = weather.city;
+      //Get & store state
+      var state = weather.region;
+      //Get & store thumbnail
+      var thumb = weather.thumbnail;
+      
+      // Output to hooks in HTML
+      $('.temp').text(temp);
+      $('.city').text(city);
+      $('.state').text(state);
+      $('.thumb img').attr('src', thumb);
+      
+      // See console for _weather_ object
+      console.log(weather);
+    },
+  
+    // if error
+    error: function(error) {  
+      $('body').html('<p>' + error + '</p>');
+    }
+  
+  });
 
 
 
+
+if (navigator.geolocation) {
+    // Yes! Show button
+    $('.getGeolocation').show(); 
+  } else {
+    // No. Hide button
+    $('.getGeolocation').hide();
+  }
+
+// 2. Get Geolocation & return Simple Weather
+$('.getGeolocation').on('click', function() {
+  
+    navigator.geolocation.getCurrentPosition(function(position) {
+    //load weather using your lat/lng coordinates. See _loadWeather()_ below
+    loadWeather(position.coords.latitude+','+position.coords.longitude); 
+    // See latitute & longitude. Note, wait a few seconds
+    console.log(position.coords.latitude+','+position.coords.longitude);
+  });
+  
+});
+
+// 3. Wrap SimpleWeather in a function called _loadWeather()
+var loadWeather = function(location) {
+    
+    $.simpleWeather({
+    location: location,
+    
+    // Get _weather_ object
+    success: function(weather) {
+        
+      console.log(weather);
+      
+      // Get & store temperature
+      var temp = weather.temp;
+      // Get & store city
+      var city = weather.city;
+      
+      // Output to hooks in HTML
+      $('.temp').text(temp);
+      $('.city').text(city);
+      
+      // See console for _weather_ object
+      console.log(weather);
+    }
+  
+  });
+    
+};
 
 
 
